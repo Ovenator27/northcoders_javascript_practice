fb0b00daf7b0909e5e7e954792101cdfdf05afdc
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2023-08-04 12:47:07 +0100

    Add explanatory comments

--- even_strings.js
+++ even_strings.js
@@ -1,13 +1,16 @@
+//Takes array of strings as input, returns an array containing only the even length strings
+
 function findEvenLengthStrings(items) {
     //create return array
     const evenStrings = [];
-    //loop through array
+    //loop through input array
     for (let i=0 ; i<items.length ; i++) {
-        //If string and even length string add to array
+        //If value is string and even length, add to return array
         if (typeof items[i] === 'string' && items[i].length % 2 === 0) {
             evenStrings.push[items[i]];
         }
     }
-    //return array
+    //print and return array
+    console.log(evenStrings);
     return evenStrings;
     }
